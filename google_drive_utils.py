3cc4bee400cdc8392c5fd958058b0c088d3abc95
Peter Devine <pdev438@aucklanduni.ac.nz>
2020-05-07 16:52:53 +1200

    Delete file after reading it to df from GD

--- google_drive_utils.py
+++ google_drive_utils.py
@@ -1,4 +1,5 @@
 import io
+import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -25,4 +26,5 @@ def get_df_from_gd_csv(file_id, file_name):
     downloaded = drive.CreateFile({'id': file_id})
     downloaded.GetContentFile(file_name)
     df = pd.read_csv(file_name)
+    os.remove(file_name)
     return df
