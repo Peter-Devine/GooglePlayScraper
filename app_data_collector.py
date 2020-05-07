3c2755209072ee4472ad12c8f674f6d533cc8b24
Peter Devine <pdev438@aucklanduni.ac.nz>
2020-05-07 17:26:23 +1200

    Add data collector to get app for collecting all app data csv into one

---
+++ app_data_collector.py
@@ -0,0 +1,26 @@
+from google_drive_utils import upload_df_to_gd, get_df_from_gd_csv, authenticate_google_drive
+import os
+
+
+drive = authenticate_google_drive()
+
+file_list = drive.ListFile({'q': "'1h2lBOSqLvzSoa5ZJ8bdrCNzqBKa823YP' in parents and trashed=false"}).GetList()
+
+file_names = [(file.attr["metadata"]["title"], file["id"]) for file in file_list]
+
+overall_df = None
+
+for file_name, file_id in file_names:
+    print(file_name)
+    try:
+        app_df = get_df_from_gd_csv(file_id, "1h2lBOSqLvzSoa5ZJ8bdrCNzqBKa823YP", file_name)
+    except Exception as err:
+        print(f"Failed by giving {err}")
+        continue
+
+    if overall_df is None:
+        overall_df = app_df
+    else:
+        overall_df = overall_df.append(app_df)
+
+upload_df_to_gd("overall_app_data.csv", overall_df, "1M8Mjk_vIVFtnFflcr60y7jD8UadMc5uk")
